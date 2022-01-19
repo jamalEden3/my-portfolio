2d3929c2c708c1f3723a009ec8607ae9cd69b56c
JamalEden <jamaleden6@gmail.com>
2022-01-19 13:12:32 +0200

    Navigation part

---
+++ src/components/header/header.jsx
@@ -0,0 +1,36 @@
+import React from 'react';
+
+
+function Header() {
+    return (
+        <header>
+            <nav>
+                <div className="logo">
+                    <a href="/" className="logo-icon">Port</a>
+                </div>
+                <div className="web-nav">
+                    <ul className="menu">
+                        <li className="nav-item">
+                            <a href="#" className="nav-link">About</a>
+                            </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link"></a>
+                            </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link"></a>
+                            </li>
+                        <li className="nav-item">
+                            <a href="#" className="nav-link"></a>
+                            </li>
+                    </ul>
+                </div>
+                <div className="mob-nav">
+                    
+                </div>
+            </nav>
+            Header
+        </header>
+    )
+}
+
+export default Header
