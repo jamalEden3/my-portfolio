981e5f8793421ab8adae15651402b7e9c380338e
JamalEden <jamaleden6@gmail.com>
2022-01-22 15:58:26 +0200

    Make navbar responsive

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './header.style.scss';
 
 import { BiMenuAltRight } from 'react-icons/bi';
@@ -6,28 +6,31 @@ import { AiOutlineClose } from 'react-icons/ai';
 import { DiYii } from "react-icons/di";
 
 function Header() {
+    const [openMenu, setOpenMenu] = useState(true);
     return (
         <header className="header">
             <div className="header__content">
-                <h2 className="header__logo"><DiYii className="logo-icon" /></h2>
-                <nav className="nav">
-                    <ul className="nav-list">
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Page One</a>
+                <h2 className="header__logo">
+                    Jamal
+                    <DiYii className="header__logo-icon" /></h2>
+                <nav className={`header__nav ${openMenu ? 'header__isMenu': ''}`}>
+                    <ul className="header__nav-list">
+                        <li className="header__nav-item">
+                            <a href="#" className="header__nav-link">About</a>
                         </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Page 2</a>
+                        <li className="header__nav-item">
+                            <a href="#" className="header__nav-link">Service</a>
                         </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Page 3</a>
+                        <li className="header__nav-item">
+                            <a href="#" className="header__nav-link">Portfolio</a>
                         </li>
-                        <li className="nav-item">
-                            <a href="#" className="nav-link">Page 4</a>
+                        <li className="header__nav-item">
+                            <a href="#" className="header__nav-link">Testimonial</a>
                         </li>
                     </ul>
-                    <button className="nav-btn btn">CTA Page</button>
+                    <button className="header__nav-btn btn">CTA Page</button>
                 </nav>
-                <div className="menu-toggle">
+                <div className="header__nav-toggle">
                     <BiMenuAltRight />
                 </div>
             </div>
