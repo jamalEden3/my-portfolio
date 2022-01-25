2f3e7e4d74df7ac52916f266f8d10da55d371ec6
JamalEden <jamaleden6@gmail.com>
2022-01-25 12:16:21 +0200

    make about's height 100vh

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import './App.style.scss';
 
 import Layout from './components/layout/layout';
 import About from './components/about/about';
+import Services from './components/services/services';
 
 function App() {
   return (
     <Layout>
       <About />
+      <Services />
     </Layout>
   );
 }
---
+++ src/components/services/services.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './services.style.scss';
+
+function Services() {
+  return (
+      <section className="services">
+        Services
+      </section>
+  );
+}
+
+export default Services;
