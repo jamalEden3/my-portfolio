a448230f0ed9ce4952516e5c3910e435e053ed12
JamalEden <jamaleden6@gmail.com>
2022-01-25 15:50:43 +0200

    finish left section of services

---
+++ src/components/services/SERVICES_DATA.js
@@ -0,0 +1,26 @@
+export const SERVICES_DATA = [
+    {
+        id: 5,
+        name: 'Web UI/UX Design',
+        number: '01/',
+
+    },
+    {
+        id: 6,
+        name: 'Mobile UI/UX Design',
+        number: '02/',
+
+    },
+    {
+        id: 7,
+        name: 'React Development - FrontEnd development',
+        number: '03/',
+
+    },
+    {
+        id: 8,
+        name: 'Node - Backend Development',
+        number: '04/',
+
+    },
+]
--- src/components/services/services.jsx
+++ src/components/services/services.jsx
@@ -1,10 +1,42 @@
 import React from 'react';
 import './services.style.scss';
 
+import { AiOutlineRise } from "react-icons/ai";
+import { FiArrowUpRight } from "react-icons/fi";
+
+import { SERVICES_DATA } from './SERVICES_DATA';
+
+
 function Services() {
   return (
       <section className="services">
-        Services
+        <div className="services__l">
+            <div className="services__infos">
+                <div className="services__info">
+                    <h2 className="services__title">Services I <br />offer
+                        <span className="services__name">Services!</span>
+                    </h2>
+                    <p className="services__subTitle">Lorem ipsum dolor sit amet, consectetur adipisicing elit. \
+                    Labore porro architecto reiciendis, deleniti maxime minima ipsa magni voluptate.</p>
+                </div>
+                <button className="services__btn">
+                    <AiOutlineRise className="services__btn-icon" />
+                </button>
+            </div>
+        </div>
+        <div className="services__r">
+            {
+                SERVICES_DATA.map(({id, name, number}) => (
+                    <div className="services__service" key={id}>
+                        <span className="services__service-num">{number}</span>
+                        <p className="services__service-name">{name}</p>
+                        <a href="#" className="services__service-link">
+                            <FiArrowUpRight />
+                        </a>
+                    </div>
+                ))
+            }
+        </div>
       </section>
   );
 }
