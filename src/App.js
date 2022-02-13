afab1e6435c3f52825309a6a07f8b11679ca4054
JamalEden <jamaleden6@gmail.com>
2022-02-13 23:41:28 +0200

    Finish About component at mobile screen

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import './App.style.scss';
 
-import Layout from './components/layout/layout';
-import About from './components/about/about';
 import Services from './components/services/services';
 import Skills from './components/skills/skills';
 
 import Header from './components/header/header';
+import About from './components/about/about';
 
 function App() {
   return (
     <>
       <Header />
+      <About />
     </>
   );
 }
--- src/components/about/about.jsx
+++ src/components/about/about.jsx
@@ -11,28 +11,31 @@ import myImg from '../../assets/me.png'
 function About() {
     return (
         <section className="about">
-            <div className="about__infos">
-                <div className="about__info">
-                    <span className="about__label">Hello!</span>
-                    <h1 className="about__title">I have passionate <br />about Front-End development</h1>
-                    <p className="about__bio">Hi! I'm FrontEnd developer - Creating bold & brave interfaces design for 
-                    companies all across the world.</p>
-                    <div className="about__btns">
-                        <button className="btn about__btn-talk">Let's Talk <AiOutlineAliwangwang className="btn-icon about__talk-icon"/></button>
-                        <button className="btn about__btn-pfl">Portflio<AiOutlineArrowRight className="btn-icon about__pfl-icon"/></button>
+            <div className="container">
+                <div className="about__infos">
+                    <div className="about__info">
+                        
+                        <h1 className="about__title">I have passionate <br />about Front-End development</h1>
+                        <p className="about__bio">Hi! I'm FrontEnd developer - Creating bold & brave interfaces design for 
+                        companies all across the world.</p>
+                        <div className="about__btns">
+                            <button className="about__btn about__btn-talk">Let's Talk <AiOutlineAliwangwang className="btn-icon about__talk-icon"/></button>
+                            <button className="about__btn about__btn-pfl">Portflio<AiOutlineArrowRight className="btn-icon about__pfl-icon"/></button>
+                        </div> {/* text div */}
+                        {/* <div className="about__social">
+                            <Social />
+                        </div> */}
+                    </div>
+                    <div className="about__image-box">
+                        <img src={myImg} alt="Jamal Icon" className="about__image" />
                     </div>
                 </div>
-                <div className="about__image-box">
-                    <img src={myImg} alt="Jamal Icon" className="about__image" />
-                </div>
             </div>
-            <div className="about__social">
-                <Social />
-            </div>
-            <div className="about__scroll">
+            
+            {/* <div className="about__scroll">
                 <div className="about__vl"></div>
                 <p>Scroll down</p>
-            </div>
+            </div> */}
         </section>
     )
 }
