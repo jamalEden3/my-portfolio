fcc579038ed83da8023dff91e7766dbc74607c27
JamalEden <jamaleden6@gmail.com>
2022-03-03 14:48:50 +0200

    apply system design to the project

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import './App.style.scss';
 
-import Header from './components/header/header';
-import About from './components/about/about';
-import Services from './components/services/services';
-import Skills from './components/skills/skills';
-import Works from './components/works/works';
-import Contacts from './components/contacts/contacts';
+
 
 function App() {
   return (
     <>
-      <Header />
-      <About />
-      <Services />
-      <Skills />
-      <Works />
-      <Contacts />
+    Website
     </>
   );
 }
